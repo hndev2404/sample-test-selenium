7eddf59102d7b3c00a38ca25577ec62912945daf
hndev2404 <hndev2404@gmail.com>
2024-12-02 03:46:38 +0700

    run test

--- tests/manager_create_course.js
+++ tests/manager_create_course.js
@@ -26,73 +26,73 @@ test.describe(common.getTestCaseName(dataManagerCreateCourse['user_type'], dataM
     test.before(async function () {
     });
 
-    test.it(dataManagerCreateCourse['case_1']['name'], async function () {
-        const driver = getDriverConfig(BROWSER);
-        await driver.get(BASE_URL);
-        await common.writeScreenshot(common.getImgs(dataManagerCreateCourse, 'case_1', 1), driver)
+    // test.it(dataManagerCreateCourse['case_1']['name'], async function () {
+    //     const driver = getDriverConfig(BROWSER);
+    //     await driver.get(BASE_URL);
+    //     await common.writeScreenshot(common.getImgs(dataManagerCreateCourse, 'case_1', 1), driver)
 
-        try {
-            await action.doLogin(dataManagerCreateCourse.account.username, dataManagerCreateCourse.account.password, driver);
-            await common.writeScreenshot(common.getImgs(dataManagerCreateCourse, 'case_1', 2), driver)
+    //     try {
+    //         await action.doLogin(dataManagerCreateCourse.account.username, dataManagerCreateCourse.account.password, driver);
+    //         await common.writeScreenshot(common.getImgs(dataManagerCreateCourse, 'case_1', 2), driver)
 
-            await goToCreateCourse(dataManagerCreateCourse['create_course_url'], driver)
-            await common.writeScreenshot(common.getImgs(dataManagerCreateCourse, 'case_1', 3), driver)
+    //         await goToCreateCourse(dataManagerCreateCourse['create_course_url'], driver)
+    //         await common.writeScreenshot(common.getImgs(dataManagerCreateCourse, 'case_1', 3), driver)
 
-            common.logAction("createCourse")
-            common.logData('fullname', dataManagerCreateCourse['case_1']['full_name'])
-            common.logData('shortname', dataManagerCreateCourse['case_1']['short_name'])
+    //         common.logAction("createCourse")
+    //         common.logData('fullname', dataManagerCreateCourse['case_1']['full_name'])
+    //         common.logData('shortname', dataManagerCreateCourse['case_1']['short_name'])
 
-            await action.findAndSendKeysById('id_fullname', dataManagerCreateCourse['case_1']['full_name'], driver)
-            await action.findAndSendKeysById('id_shortname', dataManagerCreateCourse['case_1']['short_name'], driver)
-            await common.writeScreenshot(common.getImgs(dataManagerCreateCourse, 'case_1', 4), driver)
+    //         await action.findAndSendKeysById('id_fullname', dataManagerCreateCourse['case_1']['full_name'], driver)
+    //         await action.findAndSendKeysById('id_shortname', dataManagerCreateCourse['case_1']['short_name'], driver)
+    //         await common.writeScreenshot(common.getImgs(dataManagerCreateCourse, 'case_1', 4), driver)
 
-            await action.findAndClickById('id_saveanddisplay', driver);
-            await common.writeScreenshot(common.getImgs(dataManagerCreateCourse, 'case_1', 5), driver)
+    //         await action.findAndClickById('id_saveanddisplay', driver);
+    //         await common.writeScreenshot(common.getImgs(dataManagerCreateCourse, 'case_1', 5), driver)
 
-            let errFullname = await driver.findElement(By.id('id_error_fullname'));
-            let errFullnameText = await errFullname.getText();
-            verifyErr("verifyFullnameError", dataManagerCreateCourse['case_1']['expected_err'], errFullnameText, driver)
-        } catch (err) {
+    //         let errFullname = await driver.findElement(By.id('id_error_fullname'));
+    //         let errFullnameText = await errFullname.getText();
+    //         verifyErr("verifyFullnameError", dataManagerCreateCourse['case_1']['expected_err'], errFullnameText, driver)
+    //     } catch (err) {
 
-            throw err
-        }
-        await common.sleep(5)
-        await driver.quit();
-    });
+    //         throw err
+    //     }
+    //     await common.sleep(5)
+    //     await driver.quit();
+    // });
 
-    test.it(dataManagerCreateCourse['case_2']['name'], async function () {
-        const driver = getDriverConfig(BROWSER);
-        await driver.get(BASE_URL);
-        await common.writeScreenshot(common.getImgs(dataManagerCreateCourse, 'case_2', 1), driver)
+    // test.it(dataManagerCreateCourse['case_2']['name'], async function () {
+    //     const driver = getDriverConfig(BROWSER);
+    //     await driver.get(BASE_URL);
+    //     await common.writeScreenshot(common.getImgs(dataManagerCreateCourse, 'case_2', 1), driver)
 
-        try {
-            await action.doLogin(dataManagerCreateCourse.account.username, dataManagerCreateCourse.account.password, driver);
-            await common.writeScreenshot(common.getImgs(dataManagerCreateCourse, 'case_2', 2), driver)
+    //     try {
+    //         await action.doLogin(dataManagerCreateCourse.account.username, dataManagerCreateCourse.account.password, driver);
+    //         await common.writeScreenshot(common.getImgs(dataManagerCreateCourse, 'case_2', 2), driver)
 
-            await goToCreateCourse(dataManagerCreateCourse['create_course_url'], driver)
-            await common.writeScreenshot(common.getImgs(dataManagerCreateCourse, 'case_2', 3), driver)
+    //         await goToCreateCourse(dataManagerCreateCourse['create_course_url'], driver)
+    //         await common.writeScreenshot(common.getImgs(dataManagerCreateCourse, 'case_2', 3), driver)
 
-            common.logAction("createCourse")
-            common.logData('fullname', dataManagerCreateCourse['case_2']['full_name'])
-            common.logData('shortname', dataManagerCreateCourse['case_2']['short_name'])
+    //         common.logAction("createCourse")
+    //         common.logData('fullname', dataManagerCreateCourse['case_2']['full_name'])
+    //         common.logData('shortname', dataManagerCreateCourse['case_2']['short_name'])
 
-            await action.findAndSendKeysById('id_fullname', dataManagerCreateCourse['case_2']['full_name'], driver)
-            await action.findAndSendKeysById('id_shortname', dataManagerCreateCourse['case_2']['short_name'], driver)
-            await common.writeScreenshot(common.getImgs(dataManagerCreateCourse, 'case_2', 4), driver)
+    //         await action.findAndSendKeysById('id_fullname', dataManagerCreateCourse['case_2']['full_name'], driver)
+    //         await action.findAndSendKeysById('id_shortname', dataManagerCreateCourse['case_2']['short_name'], driver)
+    //         await common.writeScreenshot(common.getImgs(dataManagerCreateCourse, 'case_2', 4), driver)
 
-            await action.findAndClickById('id_saveanddisplay', driver);
-            await common.writeScreenshot(common.getImgs(dataManagerCreateCourse, 'case_2', 5), driver)
+    //         await action.findAndClickById('id_saveanddisplay', driver);
+    //         await common.writeScreenshot(common.getImgs(dataManagerCreateCourse, 'case_2', 5), driver)
 
-            let errShortname = await driver.findElement(By.id('id_error_shortname'));
-            let errShortnameText = await errShortname.getText();
-            verifyErr("verifyShortnameError", dataManagerCreateCourse['case_2']['expected_err'], errShortnameText, driver)
-        } catch (err) {
+    //         let errShortname = await driver.findElement(By.id('id_error_shortname'));
+    //         let errShortnameText = await errShortname.getText();
+    //         verifyErr("verifyShortnameError", dataManagerCreateCourse['case_2']['expected_err'], errShortnameText, driver)
+    //     } catch (err) {
 
-            throw err
-        }
-        await common.sleep(5)
-        await driver.quit();
-    });
+    //         throw err
+    //     }
+    //     await common.sleep(5)
+    //     await driver.quit();
+    // });
 
     test.it(dataManagerCreateCourse['case_3']['name'], async function () {
         const driver = getDriverConfig(BROWSER);
@@ -130,6 +130,6 @@ test.describe(common.getTestCaseName(dataManagerCreateCourse['user_type'], dataM
             throw err
         }
         await common.sleep(5)
-        await driver.quit();
+        // await driver.quit();
     });
 })
